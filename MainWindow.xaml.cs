9da145ff9edd02fb6d8238a143cf12902d6f5eab
9thCircle <the9thcircleofhelll@gmail.com>
2024-06-09 17:47:54 +0300

    Added smoothing the bpm calibration

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,121 +1,166 @@
-﻿using System.Threading;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
-namespace CircleBeat;
-
-public partial class MainWindow : Window
+namespace CircleBeat
 {
-    private const int SLIDER_COUNT = 5;
-
-    private ulong _milisecsPassedSinceStart = 0;
-    private ulong _firstPressMili = 0;
-
-    private float MBP { get => 60000 / _bpm; set => _bpm = 60000 / value; }
-    private float _bpm = 187;
-
-    private int NowAt { get => _nowAt; set { _nowAt = value; if (_nowAt == SLIDER_COUNT + 1) _nowAt = 0; } }
-    private int _nowAt = 0;
-
-    private bool _isPlaying = false;
-
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        private const int SLIDER_COUNT = 30;
+        private const int CALIBRATION_COUNT = 30; // Number of recent intervals to consider for averaging
 
-        Media.LoadedBehavior = MediaState.Manual;
-        Media.UnloadedBehavior = MediaState.Manual;
+        private ulong _msPassedSinceStart = 0;
+        private ulong _lastPressMS = 0;
 
-        new Thread(UpdateLoop).Start();
-        KeyDown += OnKeyDown;
-    }
+        private float BPMS { get => 60000 / _bpm; set => _bpm = 60000 / value; }
+        private float _bpm = 1;
 
-    public void UpdateLoop()
-    {
-        while (true)
+        private int NowAtSlider { get => _nowAtSlider; set { _nowAtSlider = value; if (_nowAtSlider == SLIDER_COUNT) _nowAtSlider = 0; } }
+        private int _nowAtSlider = 0;
+
+        private bool _isPlaying = false;
+        private Queue<float> _recentIntervals = new Queue<float>(); // Queue to store recent calibration intervals
+
+        public MainWindow()
         {
-            _milisecsPassedSinceStart++;
+            InitializeComponent();
 
-            Dispatcher.Invoke(() =>
+            Media.LoadedBehavior = MediaState.Manual;
+            Media.UnloadedBehavior = MediaState.Manual;
+
+            new Thread(UpdateLoop).Start();
+            KeyDown += OnKeyDown;
+        }
+
+        private TimeSpan elapsed;
+        public void UpdateLoop()
+        {
+            while (true)
             {
-                BeatSldr.Value = _milisecsPassedSinceStart % MBP / (MBP / 10f);
-                CountLbl.Content = _milisecsPassedSinceStart;
-            });
+                _msPassedSinceStart += 1;
 
-            Thread.Sleep(1);
-        }
-    }
+                Dispatcher.Invoke(() =>
+                {
+                    BeatSldr.Value = _msPassedSinceStart % BPMS / (BPMS / 10f);
+                    CountLbl.Content = _msPassedSinceStart;
+                });
 
-    private void OnKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
-    {
-        if (e.Key is System.Windows.Input.Key.Escape)
-        {
-            Close();
-            return;
-        }
-        else if (e.Key is System.Windows.Input.Key.A)
-        {
-            Calibrate();
-            return;
-        }
-        else if (e.Key is System.Windows.Input.Key.P)
-        {
-            PausePlay();
-            return;
+                Thread.Sleep(1);
+            }
         }
 
-        Tap();
-    }
-
-    private void Tap()
-    {
-        Slider slider = new() { Value = _milisecsPassedSinceStart % MBP / (MBP / 10f) };
-
-        MainStack.Children.Insert(NowAt, slider);
-
-        if (MainStack.Children.Count > SLIDER_COUNT)
+        private void OnKeyDown(object sender, KeyEventArgs e)
         {
-            if (NowAt == SLIDER_COUNT)
-                MainStack.Children.RemoveAt(0);
+            if (BPMLbl.IsFocused)
+            {
+                switch (e.Key)
+                {
+                    case Key.Escape:
+                        {
+                            Keyboard.ClearFocus();
+                            return;
+                        }
+                    case Key.Enter:
+                        {
+                            BPMLbl_Completed();
+                            Keyboard.ClearFocus();
+                            return;
+                        }
+                }
+            }
             else
-                MainStack.Children.RemoveAt(NowAt + 1);
+            {
+                switch (e.Key)
+                {
+                    case Key.Escape:
+                        {
+                            Environment.Exit(0);
+                            return;
+                        }
+                    case Key.P:
+                        {
+                            PausePlay();
+                            return;
+                        }
+                    case Key.A:
+                        {
+                            Calibrate();
+                        }
+                        break;
+                }
+
+                CreateNewSlider();
+            }
         }
 
-        NowAt++;
-    }
-
-    private void PausePlay()
-    {
-        if (_isPlaying)
-            Media.Pause();
-        else
-            Media.Play();
-
-        _isPlaying = !_isPlaying;
-    }
-
-    private void Calibrate()
-    {
-        MBP = _milisecsPassedSinceStart - _firstPressMili;
-
-        BPMLbl.Content = _bpm;
-
-        _firstPressMili = _milisecsPassedSinceStart;
-    }
-
-    private void PickFile_Click(object sender, RoutedEventArgs e)
-    {
-        var openFileDialog = new Microsoft.Win32.OpenFileDialog();
-        openFileDialog.Filter = "Audio files (*.mp3, *.wav)|*.mp3;*.wav|All files (*.*)|*.*";
-
-        if (openFileDialog.ShowDialog() == true)
+        private void CreateNewSlider()
         {
-            string selectedFilePath = openFileDialog.FileName;
+            Slider slider = new() { Value = _msPassedSinceStart % BPMS / (BPMS / 10f) };
 
-            Media.Source = new(selectedFilePath);
+            SliderStack.Children.Insert(NowAtSlider, slider);
 
-            Media.Play();
-            _isPlaying = true;
+            if (SliderStack.Children.Count > SLIDER_COUNT)
+            {
+                if (NowAtSlider == SLIDER_COUNT)
+                    SliderStack.Children.RemoveAt(0);
+                else
+                    SliderStack.Children.RemoveAt(NowAtSlider + 1);
+            }
+
+            NowAtSlider++;
+        }
+
+        private void PausePlay()
+        {
+            if (_isPlaying)
+                Media.Pause();
+            else
+                Media.Play();
+
+            _isPlaying = !_isPlaying;
+        }
+
+        private void Calibrate()
+        {
+            float interval = _msPassedSinceStart - _lastPressMS;
+
+            if (_recentIntervals.Count == CALIBRATION_COUNT)
+            {
+                _recentIntervals.Dequeue();
+            }
+
+            _recentIntervals.Enqueue(interval);
+
+            // Calculate the average interval
+            float averageInterval = _recentIntervals.Average();
+            BPMS = averageInterval;
+
+            _lastPressMS = _msPassedSinceStart;
+
+            BPMLbl.Text = _bpm.ToString();
+        }
+
+        private void PickFile_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog { Filter = "Audio files (*.mp3, *.wav)|*.mp3;*.wav|All files (*.*)|*.*" };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+
+                Media.Source = new(selectedFilePath);
+                Media.Play();
+                _isPlaying = true;
+            }
+        }
+
+        private void BPMLbl_Completed()
+        {
+            float.TryParse(BPMLbl.Text, out _bpm);
         }
     }
 }
